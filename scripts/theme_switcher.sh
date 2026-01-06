c29ae6b79d9158fe31b3acfe177ab97938686611
jakmate <jakub.orzolek04@gmail.com>
2026-01-06 20:12:49 +0000

    hyprland update fixes + junky theme switcher

---
+++ scripts/theme_switcher.sh
@@ -0,0 +1,42 @@
+THEMES=("default" "mazda")
+DISPLAY_NAMES=("Default theme" "Mazda theme")
+
+OPTIONS=""
+for i in "${!THEMES[@]}"; do
+  OPTIONS+="${DISPLAY_NAMES[i]}\n"
+done
+
+SELECTED=$(echo -e "$OPTIONS" | wofi --dmenu --lines=5 --width=30%)
+
+if [[ $SELECTED == "Default theme" ]]; then
+  THEME="default"
+elif [[ $SELECTED == "Mazda theme" ]]; then
+  THEME="mazda"
+else
+  echo "No valid selection made."
+  exit 1
+fi
+
+cp "$HOME/.config/wofi/themes/$THEME.css" "$HOME/.config/wofi/style.css"
+cp "$HOME/.config/hypr/themes/hyprpaper/$THEME.conf" "$HOME/.config/hypr/hyprpaper.conf"
+cp "$HOME/.config/hypr/themes/hyprlock/$THEME.conf" "$HOME/.config/hypr/hyprlock.conf"
+cp "$HOME/.config/fastfetch/themes/$THEME.jsonc" "$HOME/.config/fastfetch/config.jsonc"
+cp "$HOME/.config/waybar/themes/$THEME.css" "$HOME/.config/waybar/style.css"
+cp "$HOME/.config/kitty/themes/$THEME.conf" "$HOME/.config/kitty/kitty.conf"
+
+# Extracting the wallpaper path from the configuration
+WALLPAPER_PATH=$(grep 'path =' "$HOME/.config/hypr/hyprpaper.conf" | cut -d'=' -f2 | tr -d ' ')
+
+# Expand the tilde to the full home path
+FULL_WALLPAPER_PATH="${WALLPAPER_PATH/#\~/$HOME}"
+
+if [[ -n $FULL_WALLPAPER_PATH ]]; then
+  hyprctl hyprpaper wallpaper "eDP-1, $FULL_WALLPAPER_PATH, cover"
+else
+  echo "Wallpaper path not found in configuration."
+fi
+
+killall waybar && waybar &
+hyprctl reload
+
+echo "Switched to $THEME theme"
--- waybar/scripts/powermenu.sh
+++ waybar/scripts/powermenu.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 # ─────────────────────────────────────────────────────────────────────────────
-#  Rofi Power Menu
+#  Wofi Power Menu
 #  Provides a simple system power menu integrated with Waybar.
 #  Example:
 #      ./powermenu.sh
-#      # Opens a Rofi menu with power options
+#      # Opens a Wofi menu with power options
 # ─────────────────────────────────────────────────────────────────────────────
 
-rofi_command="wofi -dmenu -p Power"
+wofi_command="wofi --dmenu --lines=5 --width=20%"
 
 options="Shutdown\nReboot\nLogout\nSuspend\nLock"
 
-chosen="$(echo -e "$options" | $rofi_command)"
+chosen="$(echo -e "$options" | $wofi_command)"
 case $chosen in
-    Shutdown) systemctl poweroff ;;
-    Reboot) systemctl reboot ;;
-    Logout) hyprctl dispatch exit ;;
-    Suspend) systemctl suspend ;;
-    Lock) hyprlock ;;
+Shutdown) systemctl poweroff ;;
+Reboot) systemctl reboot ;;
+Logout) hyprctl dispatch exit ;;
+Suspend) systemctl suspend ;;
+Lock) hyprlock ;;
 esac
